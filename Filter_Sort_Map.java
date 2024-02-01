856d2e8d89b2f799a1b1be80b8b9951caa0516c1
Akhil Tiwari <akhiltiwaristp@gmail.com>
2024-02-01 10:15:17 +0530

    some more examples

---
+++ Filter_Sort_Map.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.*;
+
+public class Filter_Sort_Map {
+    public static void main(String[] args) {
+        List<Integer> num = Arrays.asList(8,9,10,11,9, 3, 5);
+        
+        num.stream().filter(e-> e%2 == 0)
+                    .sorted()
+                    .map(e->e*e)
+                    .forEach(e->System.out.println(e));
+    }
+}
