3f1789bb012bcfaa83d0f8e3d0019d60053caf1f
Akhil Tiwari <akhiltiwaristp@gmail.com>
2024-02-02 11:53:37 +0530

    update on min function

--- Example.java
+++ Example.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public interface Example {
@@ -19,6 +20,24 @@ public interface Example {
         long y = list.stream().distinct().count();
         System.out.println(x);
         System.out.println(y);
+
+        list.stream().limit(2).forEach(System.out::println);
+        List<String> collect2 = list.stream().limit(3).collect(Collectors.toList());
+        System.out.println(collect2);
+        
+
+        // Finding how many numbers in the list are even numbers
+
+        List<Integer> list1 = Arrays.asList(2,3,4,5,6,7,8,13,14,16,15);
+
+        Long n = list1.stream().filter(e->e%2 == 0).count();
+        System.out.println(n);
+
+        Optional<Integer> mm = list1.stream().min((a,b)->{return a.compareTo(b);}); // use of min function
+
+        System.out.println(mm.get());
+
+
         
     }
 }
