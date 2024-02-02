f061feb02f901f920673071ad49340b422a57c78
Akhil Tiwari <akhiltiwaristp@gmail.com>
2024-02-02 11:24:36 +0530

    updated

---
+++ Example.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public interface Example {
+    public static void main(String[] args) {
+        // Non - Terminal -> filter, map, distinct, limit_
+        // return stream_of object_
+        // Terminal -> collect, count, min, max, forEach, toArray,  
+
+        List<String> list = Arrays.asList("car", "bike", "bus", "bike", "car");
+
+        List<String> collect = list.stream().distinct().collect(Collectors.toList());
+        System.out.println(collect);
+
+        list.stream().distinct().forEach(System.out::println);
+    }
+}
